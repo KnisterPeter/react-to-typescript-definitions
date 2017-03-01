8db1586cffd0cd3b923f8d5ac760386ef4bc9f56
Markus Wolf <markus.wolf@sinnerschrader.com>
2017-03-01 14:37:47 +0100

    chore: update wallaby conf to get around bug

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -4,7 +4,9 @@ module.exports = function(wallaby) {
     files: [
       'src/**/*.ts',
       {pattern: 'tests/**/*.js*', instrument: false},
-      {pattern: 'tests/**/*.d.ts', instrument: false}
+      {pattern: 'tests/**/*.d.ts', instrument: false},
+      {pattern: 'node_modules/dts-dom/package.json', instrument: false},
+      {pattern: 'node_modules/dts-dom/bin/index.d.ts', instrument: false}
     ],
     tests: [
       'tests/**/*-test.ts'
