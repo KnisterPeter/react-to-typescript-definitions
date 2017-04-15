beced3bf0d894d991dac20c8302ed7e1dc83fd11
Hendrik Liebau <mail@hendrik-liebau.de>
2017-04-16 01:18:05 +0200

    fix: handle named export specifiers
    fixes #368

---
+++ tests/named-export-specifiers.d.ts
@@ -0,0 +1,9 @@
+declare module 'component' {
+  export interface ComponentProps {
+    optionalAny?: any;
+  }
+
+  export function Component(props: ComponentProps): JSX.Element;
+
+  export function Component2(): JSX.Element;
+}
